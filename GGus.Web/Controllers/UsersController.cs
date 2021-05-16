94d1f89f6bb03cd6a0f919ac00ec5c34daf49f5a
Eynav Ptia <onevptaya@gmail.com>
2021-05-16 17:40:56 +0300

    Creating Login and Register pages. Adding Users option. Needs to finish.

---
+++ GGus.Web/Controllers/UsersController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GGus.Web.Data;
+using GGus.Web.Models;
+
+namespace GGus.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: Users/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Users/Register
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Id,Username,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var q = _context.User.FirstOrDefault(u => u.Username == user.Username);
+                if (q == null) { 
+
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index), "Home");
+                }
+                else
+                {
+                    ViewData["Error"] = "Unable to comply; Cannot register this user";
+                }
+
+            }
+            return View(user);
+        }
+
+        // GET: Users/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Users/Login
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Id,Username,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var q = from u in _context.User
+                        where u.Username == user.Username && u.Password == user.Password
+                        select u;
+                
+                if (q.Count() > 0)
+                {
+
+                    
+                    return RedirectToAction(nameof(Index), "Home");
+                }
+                else
+                {
+                    ViewData["Error"] = "Username and/or password are incorrect.";
+                }
+
+            }
+            return View(user);
+        }
+
+
+        /*     // GET: Users
+             public async Task<IActionResult> Index()
+             {
+                 return View(await _context.User.ToListAsync());
+             }
+
+             // GET: Users/Details/5
+             public async Task<IActionResult> Details(int? id)
+             {
+                 if (id == null)
+                 {
+                     return NotFound();
+                 }
+
+                 var user = await _context.User
+                     .FirstOrDefaultAsync(m => m.Id == id);
+                 if (user == null)
+                 {
+                     return NotFound();
+                 }
+
+                 return View(user);
+             }
+
+
+
+             // GET: Users/Edit/5
+             public async Task<IActionResult> Edit(int? id)
+             {
+                 if (id == null)
+                 {
+                     return NotFound();
+                 }
+
+                 var user = await _context.User.FindAsync(id);
+                 if (user == null)
+                 {
+                     return NotFound();
+                 }
+                 return View(user);
+             }
+
+             // POST: Users/Edit/5
+             // To protect from overposting attacks, enable the specific properties you want to bind to.
+             // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+             [HttpPost]
+             [ValidateAntiForgeryToken]
+             public async Task<IActionResult> Edit(int id, [Bind("Id,Username,Password,Type")] User user)
+             {
+                 if (id != user.Id)
+                 {
+                     return NotFound();
+                 }
+
+                 if (ModelState.IsValid)
+                 {
+                     try
+                     {
+                         _context.Update(user);
+                         await _context.SaveChangesAsync();
+                     }
+                     catch (DbUpdateConcurrencyException)
+                     {
+                         if (!UserExists(user.Id))
+                         {
+                             return NotFound();
+                         }
+                         else
+                         {
+                             throw;
+                         }
+                     }
+                     return RedirectToAction(nameof(Index));
+                 }
+                 return View(user);
+             }
+
+             // GET: Users/Delete/5
+             public async Task<IActionResult> Delete(int? id)
+             {
+                 if (id == null)
+                 {
+                     return NotFound();
+                 }
+
+                 var user = await _context.User
+                     .FirstOrDefaultAsync(m => m.Id == id);
+                 if (user == null)
+                 {
+                     return NotFound();
+                 }
+
+                 return View(user);
+             }
+
+             // POST: Users/Delete/5
+             [HttpPost, ActionName("Delete")]
+             [ValidateAntiForgeryToken]
+             public async Task<IActionResult> DeleteConfirmed(int id)
+             {
+                 var user = await _context.User.FindAsync(id);
+                 _context.User.Remove(user);
+                 await _context.SaveChangesAsync();
+                 return RedirectToAction(nameof(Index));
+             }
+
+             private bool UserExists(int id)
+             {
+                 return _context.User.Any(e => e.Id == id);
+             }*/
+    }
+}
---
+++ GGus.Web/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GGus.Web.Models
+{
+    public enum UserType
+    {
+        Client,
+        Admin
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public UserType Type { get; set; } = UserType.Client;
+    }
+}
